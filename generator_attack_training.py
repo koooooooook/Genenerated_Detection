3a909635b402573f7212eaf4e4f34d75eca15e93
koooooooook <kojunewon@kaist.ac.kr>
2024-05-01 16:16:34 +0000

    wandb name fixed

--- generator_attack_training.py
+++ generator_attack_training.py
@@ -40,7 +40,7 @@ pretrained_generator = 'scratch' # pretrained, scratch
 target_image_path = f'{train_with}_{train_id}.png'
 # wandb
 wandb_project = 'GenDet'
-wandb_name = f'progan_{pretrained_generator}_{train_id}'
+wandb_name = f'progan_{pretrained_generator}_{train_with}_{train_id}'
 log_freq=10
 # detector
 det_arch='CLIP:ViT-L/14' # 'Imagenet:resnet18','Imagenet:resnet34','Imagenet:resnet50','Imagenet:resnet101','Imagenet:resnet152','Imagenet:vgg11','Imagenet:vgg19','Imagenet:swin-b','Imagenet:swin-s','Imagenet:swin-t','Imagenet:vit_b_16','Imagenet:vit_b_32','Imagenet:vit_l_16','Imagenet:vit_l_32','CLIP:RN50','CLIP:RN101','CLIP:RN50x4','CLIP:RN50x16','CLIP:RN50x64','CLIP:ViT-B/32','CLIP:ViT-B/16','CLIP:ViT-L/14','CLIP:ViT-L/14@336px',
